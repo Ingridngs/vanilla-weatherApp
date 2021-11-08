3d80bddefb6f3244f75008592dafaefeb21930e5
Ingrid <ingrid.gs@hotmail.com>
2021-11-07 20:30:48 -0600

    Added icons

--- src/app.js
+++ src/app.js
@@ -38,6 +38,12 @@ function showTemperature(response) {
     dataHumidity.innerHTML = response.data.main.humidity
     let dataDate = document.querySelector('#lastupdate')
     dataDate.innerHTML = formatDate(response.data.dt * 1000)
+    let iconElement = document.querySelector('#icon')
+    iconElement.setAttribute(
+        'src',
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    )
+    iconElement.setAttribute('alt', response.data.weather[0].description)
 }
 
 function search(city) {
